9364e0ec8007e9665bbf61633d83cff938e9800b
vetneka <vetneka@yandex.ru>
2021-08-11 16:36:32 +0500

    (review)refactor: change app initialization

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,6 @@ import {
   useHistory,
 } from 'react-router-dom';
 
-import { I18nextProvider } from 'react-i18next';
-
-import { Provider as RollbarProvider, ErrorBoundary, LEVEL_WARN } from '@rollbar/react';
-
-import { Provider } from 'react-redux';
-
 import Chat from './pages/Chat.jsx';
 import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
@@ -29,11 +23,7 @@ import Header from './components/Header.jsx';
 import MainContainer from './components/MainContainer.jsx';
 import Footer from './components/Footer.jsx';
 
-import { authContext, socketContext } from './contexts';
-
-import store from './store';
-import { actions as channelsInfoActions } from './slices/channelsInfoSlice';
-import { actions as messagesInfoActions } from './slices/messagesInfoSlice';
+import { authContext } from './contexts';
 
 import { useAuth } from './hooks';
 
@@ -86,85 +76,30 @@ const PrivateRoute = ({ children, path }) => {
   );
 };
 
-const SocketProvider = ({ socket, children }) => {
-  const acknowledgeWithTimeout = (onSuccess, onTimeout, timeout) => {
-    const status = {
-      isCalled: false,
-    };
-
-    const timerId = setTimeout(() => {
-      if (status.isCalled) return;
-      status.isCalled = true;
-      onTimeout();
-    }, timeout);
-
-    return (...args) => {
-      if (status.isCalled) return;
-      status.isCalled = true;
-      clearTimeout(timerId);
-      onSuccess(args);
-    };
-  };
-
-  socket.on('newMessage', (message) => {
-    store.dispatch(messagesInfoActions.addMessage(message));
-  });
-
-  socket.on('newChannel', (channel) => {
-    store.dispatch(channelsInfoActions.addChannel(channel));
-  });
-
-  socket.on('removeChannel', ({ id }) => {
-    store.dispatch(channelsInfoActions.removeChannel(id));
-  });
-
-  socket.on('renameChannel', (channel) => {
-    store.dispatch(channelsInfoActions.renameChannel(channel));
-  });
-
-  return (
-    <socketContext.Provider value={{ socket, acknowledgeWithTimeout }}>
-      {children}
-    </socketContext.Provider>
-  );
-};
-
-const ErrorBoundaryPage = () => <NotFound />;
-
-const App = ({ socketClient, i18nInstance, rollbarConfig }) => (
+const App = () => (
   <Router>
-    <RollbarProvider config={rollbarConfig}>
-      <ErrorBoundary level={LEVEL_WARN} fallbackUI={ErrorBoundaryPage}>
-        <Provider store={store}>
-          <SocketProvider socket={socketClient}>
-            <AuthProvider>
-              <I18nextProvider i18n={i18nInstance}>
-                <Header />
+    <AuthProvider>
+      <Header />
 
-                <MainContainer>
-                  <Switch>
-                    <Route path="/login">
-                      <Login />
-                    </Route>
-                    <Route path="/signup">
-                      <SignUp />
-                    </Route>
-                    <PrivateRoute exact path="/">
-                      <Chat />
-                    </PrivateRoute>
-                    <Route path="*">
-                      <NotFound />
-                    </Route>
-                  </Switch>
-                </MainContainer>
+      <MainContainer>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <PrivateRoute exact path="/">
+            <Chat />
+          </PrivateRoute>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </MainContainer>
 
-                <Footer />
-              </I18nextProvider>
-            </AuthProvider>
-          </SocketProvider>
-        </Provider>
-      </ErrorBoundary>
-    </RollbarProvider>
+      <Footer />
+    </AuthProvider>
   </Router>
 );
 
--- src/init.jsx
+++ src/init.jsx
@@ -1,14 +1,70 @@
 // @ts-check
 
 import React from 'react';
+import { Provider as StoreProvider } from 'react-redux';
 
 import { io } from 'socket.io-client';
+
+import { Provider as RollbarProvider, ErrorBoundary, LEVEL_WARN } from '@rollbar/react';
 
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import resources from './locales';
 
+import store from './store';
+import { actions as channelsInfoActions } from './slices/channelsInfoSlice';
+import { actions as messagesInfoActions } from './slices/messagesInfoSlice';
+
+import { socketContext } from './contexts';
+
+import NotFound from './pages/NotFound.jsx';
+
 import App from './App.jsx';
+
+const SocketProvider = ({ socket, children }) => {
+  const acknowledgeWithTimeout = (onSuccess, onTimeout, timeout) => {
+    const status = {
+      isCalled: false,
+    };
+
+    const timerId = setTimeout(() => {
+      if (status.isCalled) return;
+      status.isCalled = true;
+      onTimeout();
+    }, timeout);
+
+    return (...args) => {
+      if (status.isCalled) return;
+      status.isCalled = true;
+      clearTimeout(timerId);
+      onSuccess(args);
+    };
+  };
+
+  socket.on('newMessage', (message) => {
+    store.dispatch(messagesInfoActions.addMessage(message));
+  });
+
+  socket.on('newChannel', (channel) => {
+    store.dispatch(channelsInfoActions.addChannel(channel));
+  });
+
+  socket.on('removeChannel', ({ id }) => {
+    store.dispatch(channelsInfoActions.removeChannel(id));
+  });
+
+  socket.on('renameChannel', (channel) => {
+    store.dispatch(channelsInfoActions.renameChannel(channel));
+  });
+
+  return (
+    <socketContext.Provider value={{ socket, acknowledgeWithTimeout }}>
+      {children}
+    </socketContext.Provider>
+  );
+};
+
+const ErrorBoundaryPage = () => <NotFound />;
 
 const init = async (socketClient = io()) => {
   const rollbarConfig = {
@@ -29,20 +85,23 @@ const init = async (socketClient = io()) => {
   };
 
   const defaultLocale = 'ru';
-  const i18nInstance = i18n.createInstance();
 
-  await i18nInstance.use(initReactI18next).init({
+  await i18n.use(initReactI18next).init({
     lng: defaultLocale,
     debug: false,
     resources,
   });
 
   return (
-    <App
-      socketClient={socketClient}
-      i18nInstance={i18nInstance}
-      rollbarConfig={rollbarConfig}
-    />
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary level={LEVEL_WARN} fallbackUI={ErrorBoundaryPage}>
+        <StoreProvider store={store}>
+          <SocketProvider socket={socketClient}>
+            <App />
+          </SocketProvider>
+        </StoreProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
